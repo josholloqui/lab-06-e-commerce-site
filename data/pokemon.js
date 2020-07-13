a34c30bf5d6d0390f2afa2fd706cc9a7338803ef
Frank Josh Olloqui <josh.olloqui@gmail.com>
2020-07-13 16:04:44 -0700

    add pokemon product

---
+++ data/pokemon.js
@@ -0,0 +1,80 @@
+const charizard = {
+    id: 'pkn001',
+    name: 'Charizard',
+    image: 'charizard.png',
+    description: 'Charizard breathes fire of such massive heat that it can melt anything. They also love to roam the skys. Fire and Flying type.',
+    category: 'fire-flying',
+    price: 540
+};
+
+const squirtle = {
+    id: 'pkn002',
+    name: 'Squirtle',
+    image: 'squirtle.png',
+    description: 'Squirtle has a shell that is not only used for protection, but also its design helps minimize resistance in water. Thus, being able to manuever at high speeds in the water.',
+    category: 'water',
+    price: 260
+};
+
+const budew = {
+    id: 'pkn003',
+    name: 'Budew',
+    image: 'budew.png',
+    description: 'Budew is an adorable companion, but they also can protect you with poison abilities. Loves being by your side.',
+    category: 'grass-poison',
+    price: 112
+};
+
+const wailord = {
+    id: 'pkn004',
+    name: 'Wailord',
+    image: 'wailord.png',
+    description: 'Wailords tend to eat a lot of fish, so you must be careful since fishermen tend to try to chase them off. They can also be used as a personal boat.',
+    category: 'water',
+    price: 198
+};
+
+const slowking = {
+    id: 'pkn005',
+    name: 'Slowking',
+    image: 'slowking.png',
+    description: 'Slowking constantly comes up with new ideas that would change the world, but sadly as soon as it hits upon a new idea, it forgets it.',
+    category: 'water-psychic',
+    price: 999
+};
+
+const ditto = {
+    id: 'pkn006',
+    name: 'Ditto',
+    image: 'ditto.png',
+    description: 'Ditto can transform into anything, but keep in mind each Ditto apparently has its own strengths and weaknesses when it comes to transformations.',
+    category: 'normal',
+    price: 10
+};
+
+const mew = {
+    id: 'pkn007',
+    name: 'Mew',
+    image: 'mew.png',
+    description: 'Mew is said to possess the genetic composition of all Pokémon. It is capable of making itself invisible at will, so it entirely avoids notice even if it approaches people.',
+    category: 'psychic',
+    price: 1000000
+};
+
+const rayquaza = {
+    id: 'pkn008',
+    name: 'Rayquaza',
+    image: 'rayquaza.png',
+    description: 'Rayquaza is said to have lived for hundreds of millions of years. Legends remain of how it put to rest the clash between Kyogre and Groudon.',
+    category: 'dragon-flying',
+    price: 640000
+};
+
+const celebi = {
+    id: 'pkn009',
+    name: 'Celebi',
+    image: 'celebi.png',
+    description: 'Celebi came from the future by crossing over time. It is thought that so long as Celebi appears, a bright and shining future awaits us.',
+    category: 'psychic-grass',
+    price: 760000
+};
