02401c2af9c8f4fd1a1a7313805d708f3b3ecd71
Frank Josh Olloqui <josh.olloqui@gmail.com>
2020-07-14 13:48:04 -0700

    adds cart data

---
+++ data/cart.js
@@ -0,0 +1,18 @@
+const cart = [{
+    id: 'pkn001',
+    quantity: 2
+}, {
+    id: 'pkn003',
+    quantity: 3
+}, {
+    id: 'pkn006',
+    quantity: 10
+}, {
+    id: 'pkn007',
+    quantity: 1
+}, {
+    id: 'pkn009',
+    quantity: 1
+}];
+
+export default cart;
