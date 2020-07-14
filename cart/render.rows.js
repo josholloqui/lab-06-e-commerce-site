74fff237878722c44bdbb9cfbb42af7f7ab00400
Frank Josh Olloqui <josh.olloqui@gmail.com>
2020-07-14 13:29:34 -0700

    adds html design and framwork

---
+++ cart/render.rows.js
@@ -0,0 +1,9 @@
+
+
+export function renderRows(rowItem, pokemon) {
+    const tableRow = document.createElement('tr');
+
+    const nameData = document.createElement('td');
+    nameData.classList.add = 'align-left';
+    
+}
