e5dba09d8c2bb68ffdd01267c2321fd8a6a88536
Frank Josh Olloqui <josh.olloqui@gmail.com>
2020-07-17 00:44:21 -0700

    adds password to enter admin

---
+++ login/login.js
@@ -0,0 +1,9 @@
+function passwordCheck(){
+    let password = prompt('Enter password to access Admin');
+    if (password === 'pokemonmaster') {
+        window.location = '../admin/index.html';
+    } else if (password !='' && password!=null) {
+        window.location = '../';
+    }
+}
+window.onload=passwordCheck;
